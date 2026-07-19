f72eba1c72521253668b7383a84ed653688baff6
Xuân Lâm <hoangxuanlam2007@outlook.com>
2026-07-19 22:50:04 +0700

    Thêm dependency cho package & Tạo index + vite config

---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import electron from 'vite-plugin-electron/simple';
+import path from 'node:path';
+
+// Vite + React + Electron 
+// P/S: Quên không dùng React, giờ mất công chuyển sang =)))
+export default defineConfig({
+  plugins: [
+    react(),
+    electron({
+      main: {
+        entry: 'electron/main.ts',
+        vite: {
+          build: {
+            outDir: 'dist-electron',
+            rollupOptions: {
+              external: ['electron', 'dotenv'],
+            },
+          },
+        },
+      },
+      preload: {
+        input: path.join(__dirname, 'electron/preload.ts'),
+        vite: {
+          build: {
+            outDir: 'dist-electron',
+            rollupOptions: {
+              output: {
+                format: 'cjs',
+                entryFileNames: 'preload.cjs',
+              },
+            },
+          },
+        },
+      },
+      renderer: {},
+    }),
+  ],
+});
