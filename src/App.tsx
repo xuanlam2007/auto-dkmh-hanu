d7956ba7385aa1026d59e7aac6a115531ad0a36f
Xuân Lâm <hoangxuanlam2007@outlook.com>
2026-07-20 15:03:06 +0700

    Thêm Main và App component cho src | Update .gitignore

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>Auto DKMH GUI</h1>
+        <p className="subtitle">Tìm học phần, chọn các nhóm muốn tự động đăng ký và cấu hình thông tin đăng nhập.</p>
+      </header>
+
+      <div className="tabs">
+        <button>Tìm môn học</button>
+        <button>Đăng nhập</button>
+        <button>Thông tin đăng ký</button>
+      </div>
+    </div>
+  );
+}
